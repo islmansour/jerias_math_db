9dd6bd886278c6f437aaf982db0f18c5e523f430
islmansour <Islmansour@gmail.com>
2023-07-26 11:50:29 +0300

    add new days of week field to group for bit troggle

--- jerias_production/models.py
+++ jerias_production/models.py
@@ -77,6 +77,7 @@ class Group(models.Model):
     weekDays = models.CharField(max_length=255)
     type = models.IntegerField(null=True)
     status = models.IntegerField(null=True)
+    days_of_week = models.IntegerField(default=0)
     createdBy = models.OneToOneField(
         'Person',
         null=True,
